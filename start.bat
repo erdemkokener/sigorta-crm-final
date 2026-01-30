c286d34e4ed31ece68153360c60bd8687dc6dda0
erdemkokener <erdemkokener@gmail.com>
2026-01-30 12:09:01 +0300

    SMTP guvenlik guncellemesi ve hatirlatma sistemi iyilestirmeleri

--- start.bat
+++ start.bat
@@ -8,13 +8,15 @@ set SMTP_HOST=smtp.gmail.com
 set SMTP_PORT=587
 set SMTP_SECURE=false
 set SMTP_USER=erdemkokener@gmail.com
-set SMTP_PASS=bznfvszqqhasczon
+set SMTP_PASS=
+
 set MAIL_FROM=erdemkokener@gmail.com
 set MAIL_TO=erdemkokener@gmail.com
 set DATA_FILE=C:\Users\PC\OneDrive\Belgeler\sigortacrmfinal\local-data.json
 :: MongoDB Bağlantısı (İsteğe bağlı - Yerelde bulut veritabanını kullanmak için)
 :: set MONGODB_URI=mongodb+srv://erdemkokener:<db_password>@cluster0.wsaqpek.mongodb.net/?appName=Cluster0
-set EMERGENCY_RESET_CODE=erdemAcil2025!
+set EMERGENCY_RESET_CODE=erdem
+
 set ALLOW_RESET=false
  
 echo Starting Sigorta CRM...
